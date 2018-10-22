930fea49c17fb3c3ca802a9023d01b9cd8a6917d
Ruslan16021984 <carbit3333333@gmail.com>
2018-10-22 14:40:41 +0300

    part2 add buttons and editText

--- app/src/main/java/carbit3333333/gmail/com/moneytracker/MainActivity.java
+++ app/src/main/java/carbit3333333/gmail/com/moneytracker/MainActivity.java
@@ -2,12 +2,50 @@ package carbit3333333.gmail.com.moneytracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText name;
+    private EditText price;
+    private Button addBtn;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         name = findViewById(R.id.name);
+        price = findViewById(R.id.price);
+        addBtn = findViewById(R.id.add_btn);
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+               addBtn.setEnabled(TextUtils.isEmpty(editable));
+            }
+        });
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String itemName = name.getText().toString();
+                String itemPrice = name.getText().toString();
+            }
+        });
+
     }
 }
